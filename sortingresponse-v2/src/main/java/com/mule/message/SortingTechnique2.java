080dc7ca96f64a73679d22a13b3f80d46ac83beb
yeshwanth-nadakuditi <yeshwanth.nadakuditi@gmail.com>
2017-02-16 13:57:31 +0530

    Added new Technique

--- sortingresponse-v2/src/main/java/com/mule/message/SortingTechnique2.java
+++ sortingresponse-v2/src/main/java/com/mule/message/SortingTechnique2.java
@@ -1,5 +1,14 @@
 package com.mule.message;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.mule.DefaultMuleEvent;
 import org.mule.api.MuleEvent;
 import org.mule.api.MuleException;
 import org.mule.api.routing.AggregationContext;
@@ -7,10 +16,54 @@ import org.mule.routing.AggregationStrategy;
 
 public class SortingTechnique2 implements AggregationStrategy {
 
+	private  List<JSONObject> jsonValues = new ArrayList<JSONObject>();
+	private  JSONObject jsonResp = new JSONObject();
+	private JSONArray jsonArr = new JSONArray();
+	private JSONObject jsonObj = new JSONObject();
+	
 	@Override
 	public MuleEvent aggregate(AggregationContext context) throws MuleException {
 		// TODO Auto-generated method stub
-		return null;
+		MuleEvent result = null;
+		for (MuleEvent event : context.collectEventsWithoutExceptions()) {
+			String response = "";
+			try {
+				response = (String) event.getMessage().getPayloadAsString();
+				jsonObj = new JSONObject(response);
+				jsonArr = jsonObj.getJSONArray("records");
+				for (int j = 0; j < jsonArr.length(); j++) {
+					jsonValues.add(jsonArr.getJSONObject(j));
+				}
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+			if (result == null){
+				result = DefaultMuleEvent.copy(event);
+				result.getMessage().setPayload("");
+			}
+		}
+		System.out.println(jsonValues);
+		Collections.sort(jsonValues, new Comparator<JSONObject>() {
+			private static final String KEY_NAME = "riCountryCode";
+			@Override
+			public int compare(JSONObject a, JSONObject b) {
+				String valA = new String();
+				String valB = new String();
+				try {
+					 valA = (String) a.get(KEY_NAME);
+					 valB = (String) b.get(KEY_NAME);
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+				return valA.compareTo(valB);
+			}
+		});
+		if (result != null) {
+			jsonResp.put("SortedRecords", jsonValues);
+			result.getMessage().setPayload(jsonResp.toString());
+			return result;
+		}
+		throw new RuntimeException("no response obtained");
 	}
-
 }
---
+++ sortingresponse-v2/src/main/java/com/mule/message/Sortingtechnique3.java
@@ -0,0 +1,56 @@
+package com.mule.message;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.mule.DefaultMuleEvent;
+import org.mule.api.MuleEvent;
+import org.mule.api.MuleException;
+import org.mule.api.routing.AggregationContext;
+import org.mule.routing.AggregationStrategy;
+
+public class Sortingtechnique3 implements AggregationStrategy {
+	private JSONObject jsonObj = new JSONObject();
+	private JSONObject jsonResp = new JSONObject();
+	private JSONArray jsonArr = new JSONArray();
+	private  List<JSONObject> jsonValues = new ArrayList<JSONObject>();
+	@Override
+	public MuleEvent aggregate(AggregationContext context) throws MuleException {
+		// TODO Auto-generated method stub
+		MuleEvent result = null;
+		for (MuleEvent event : context.collectEventsWithoutExceptions()) {
+			String response = "";
+			try {
+				/*Concurrent Modification exception was due to parsing Mule event to JSON Obj*/
+				response = (String) event.getMessage().getPayloadAsString();
+				jsonArr = appendJSONArray(response);
+				
+				/* Using JSON Arrays cannot better the performance at all
+				 * srcjsonArr =  jsonObj.getJSONArray("records");
+				for (int i = 1; i < srcjsonArr.length(); i++) {
+					destjsonArr.put(srcjsonArr.getJSONObject(i));
+					//System.out.println(destjsonArr);
+				}*/
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			for (int j = 0; j < jsonArr.length(); j++) {
+				jsonValues.add(jsonArr.getJSONObject(j));
+			}
+			if (result == null){
+			result = DefaultMuleEvent.copy(event);
+			}
+		}
+		jsonResp.put("SortedRecords", jsonValues);
+		result.getMessage().setPayload(jsonResp.toString());
+		return result;
+	}
+	
+	public synchronized JSONArray appendJSONArray(String response){
+		jsonObj = new JSONObject(response);
+		jsonArr = jsonObj.getJSONArray("records");
+		return jsonArr;
+	}
+}
